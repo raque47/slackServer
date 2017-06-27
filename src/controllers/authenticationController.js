89b31f988aa0dea9c85d49ca0aaf608effd9a5a5
raque47 <raquelchava_94@hotmail.com>
2017-06-27 06:57:04 -0600

    changes in authentication photo of user

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,27 +1,29 @@
 const jwt = require('jsonwebtoken'),
-    crypto = require('crypto'),
-    User = require('../models/User'),
-    config = require('../../config/main');
+  crypto = require('crypto'),
+  User = require('../models/User'),
+  config = require('../../config/main');
 
 function generateToken(user) {
-    return jwt.sign(user, config.secret, {
-        expiresIn: 10080 // in seconds
-    });
+  return jwt.sign(user, config.secret, {
+    expiresIn: 10080 // in seconds
+  });
 }
 
 
 // Set user info from request
-function setUserInfo(request) {  
+function setUserInfo(request) {
   return {
     _id: request._id,
     firstName: request.profile.firstName,
     lastName: request.profile.lastName,
-    email: request.email  };
+    email: request.email,
+    photo: request.photo,
+  };
 }
 
 
 // Login Route
-exports.login = function(req, res, next) {
+exports.login = function (req, res, next) {
   let userInfo = setUserInfo(req.user);
   res.status(200).json({
     token: 'JWT ' + generateToken(userInfo),
@@ -31,49 +33,49 @@ exports.login = function(req, res, next) {
 
 
 // Registration 
-exports.register = function(req, res, next) {  
+exports.register = function (req, res, next) {
   // Check for registration errors
   const email = req.body.email;
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const password = req.body.password;
   const photo = req.body.photo;
-  
+
   // Return error if no email provided
   if (!email) {
-    return res.status(422).send({ error: 'You must enter an email address.'});
+    return res.status(422).send({ error: 'You must enter an email address.' });
   }
   // Return error if full name not provided
   if (!firstName || !lastName) {
-    return res.status(422).send({ error: 'You must enter your full name.'});
+    return res.status(422).send({ error: 'You must enter your full name.' });
   }
   // Return error if no password provided
   if (!password) {
     return res.status(422).send({ error: 'You must enter a password.' });
   }
 
-  User.findOne({ email: email }, function(err, existingUser) {
+  User.findOne({ email: email }, function (err, existingUser) {
+    if (err) { return next(err); }
+    // If user is not unique, return error
+    if (existingUser) {
+      return res.status(422).send({ error: 'That email is already in use.' });
+    }
+
+    let user = new User({
+      email: email,
+      password: password,
+      profile: { firstName: firstName, lastName: lastName },
+      photo: photo
+    });
+
+    user.save(function (err, user) {
       if (err) { return next(err); }
-      // If user is not unique, return error
-      if (existingUser) {
-        return res.status(422).send({ error: 'That email is already in use.' });
-      }
-
-      let user = new User({
-        email: email,
-        password: password,
-        photo: photo,
-        profile: { firstName: firstName, lastName: lastName }
+      let userInfo = setUserInfo(user);
+      res.status(201).json({
+        token: 'JWT ' + generateToken(userInfo),
+        user: userInfo
       });
-
-      user.save(function(err, user) {
-        if (err) { return next(err); }
-        let userInfo = setUserInfo(user);
-        res.status(201).json({
-          token: 'JWT ' + generateToken(userInfo),
-          user: userInfo
-        });
-      });
+    });
   });
 
 }
